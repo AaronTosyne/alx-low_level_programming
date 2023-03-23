36f4c53ea2870aac7a816beca3f98043174e4443
AaronT <abiolatosin3@gmail.com>
2023-03-23 03:31:10 -0700

    task 4

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * file 4 - defininf sum
+ * desc: the header file will contain
+ * a function that addtwo int x and y
+ */
+
+#define int SUM(x, y) ((x) + (y))
+
+#endif
