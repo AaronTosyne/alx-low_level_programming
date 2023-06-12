15127823c8850c8ebf0e7c2ca2ec0c898138a310
AaronTosyne <abiolatosin3@gmail.com>
2023-06-12 08:11:13 -0700

    A file that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* @agrc: count no of arguments
+ * @argv: An array of arguments
+ *
+ * returns 0 when succesful
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("The name of this file is: %s\n",argv[1]);
+
+	return 0;
+}
